59e9c0f4b4399b4f641732b0abcfefaa679c8ba6
dileep-lgtm <dileep@deepfacts.io>
2025-09-17 12:21:48 +0530

    Name change

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bluetooth Demo',
+      title: 'Bluetooth V3',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const BluetoothDeviceListScreen(),
     );
